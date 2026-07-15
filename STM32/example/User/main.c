b392f4fdd724523edebc6eca638d1ea63379c910
A1RER <198746840+A1RER@users.noreply.github.com>
2026-07-15 10:57:42 +0800

    Persist waveform settings

--- STM32/example/User/main.c
+++ STM32/example/User/main.c
@@ -12,6 +12,14 @@
 #define DEFAULT_FREQ    1000
 #define DEFAULT_DUTY    50
 
+#define SETTINGS_FLASH_ADDR       0x0800FC00
+#define SETTINGS_MAGIC            0x5347
+#define SETTINGS_VERSION          0x0001
+#define SETTINGS_SAVE_IDLE_LOOPS  50
+
+static uint8_t settings_dirty = 0;
+static uint16_t settings_save_countdown = 0;
+
 static void LED_PC13_Init(void)
 {
     GPIO_InitTypeDef gpio;
@@ -76,6 +84,142 @@ static void LCD_Show_Status(void)
     }
 }
 
+static uint16_t Settings_Checksum(const uint16_t *data)
+{
+    uint16_t checksum;
+
+    checksum = 0xA55A;
+    checksum ^= data[0];
+    checksum ^= data[1];
+    checksum ^= data[2];
+    checksum ^= data[3];
+    checksum ^= data[4];
+    checksum ^= data[5];
+
+    return checksum;
+}
+
+static void Settings_MarkDirty(void)
+{
+    settings_dirty = 1;
+    settings_save_countdown = SETTINGS_SAVE_IDLE_LOOPS;
+}
+
+static void Settings_SaveNow(void)
+{
+    uint16_t data[7];
+    uint8_t i;
+    uint32_t address;
+    FLASH_Status status;
+
+    data[0] = SETTINGS_MAGIC;
+    data[1] = SETTINGS_VERSION;
+    data[2] = (uint16_t)current_wave;
+    data[3] = (uint16_t)wave_freq;
+    data[4] = (uint16_t)square_duty;
+    data[5] = (uint16_t)wave_output_enabled;
+    data[6] = Settings_Checksum(data);
+
+    FLASH_Unlock();
+    status = FLASH_ErasePage(SETTINGS_FLASH_ADDR);
+
+    if (status == FLASH_COMPLETE)
+    {
+        address = SETTINGS_FLASH_ADDR;
+
+        for (i = 0; i < 7; i++)
+        {
+            status = FLASH_ProgramHalfWord(address, data[i]);
+            if (status != FLASH_COMPLETE)
+            {
+                break;
+            }
+
+            address += 2;
+        }
+    }
+
+    FLASH_Lock();
+
+    if (status == FLASH_COMPLETE)
+    {
+        settings_dirty = 0;
+    }
+}
+
+static void Settings_Task(void)
+{
+    if (settings_dirty == 0)
+    {
+        return;
+    }
+
+    if (settings_save_countdown > 0)
+    {
+        settings_save_countdown--;
+        return;
+    }
+
+    Settings_SaveNow();
+}
+
+static uint8_t Settings_Load(void)
+{
+    const uint16_t *data;
+    WaveTypeDef saved_wave;
+    uint32_t saved_freq;
+    uint8_t saved_duty;
+    uint8_t saved_output;
+
+    data = (const uint16_t *)SETTINGS_FLASH_ADDR;
+
+    if (data[0] != SETTINGS_MAGIC)
+    {
+        return 0;
+    }
+
+    if (data[1] != SETTINGS_VERSION)
+    {
+        return 0;
+    }
+
+    if (data[6] != Settings_Checksum(data))
+    {
+        return 0;
+    }
+
+    if (data[2] >= WAVE_MAX)
+    {
+        return 0;
+    }
+
+    saved_freq = data[3];
+    if (saved_freq < FREQ_MIN || saved_freq > FREQ_MAX)
+    {
+        return 0;
+    }
+
+    if (data[4] < DUTY_MIN || data[4] > DUTY_MAX)
+    {
+        return 0;
+    }
+
+    saved_wave = (WaveTypeDef)data[2];
+    saved_duty = (uint8_t)data[4];
+    saved_output = (uint8_t)(data[5] ? 1 : 0);
+
+    Wave_SetType(saved_wave);
+    Wave_SetFreq(saved_freq);
+    Wave_SetDuty(saved_duty);
+    Wave_SetOutput(saved_output);
+    Wave_ResetIndex();
+
+    settings_dirty = 0;
+    settings_save_countdown = 0;
+
+    return 1;
+}
+
 static void Wave_Load_Default_Param(void)
 {
     Wave_SetFreq(DEFAULT_FREQ);
@@ -122,10 +266,14 @@ int main(void)
     Wave_Init();
     TIM2_Init();
 
-    Wave_SetType(WAVE_SQUARE);
-    Wave_SetFreq(DEFAULT_FREQ);
-    Wave_SetDuty(DEFAULT_DUTY);
-    Wave_ResetIndex();
+    if (Settings_Load() == 0)
+    {
+        Wave_SetType(WAVE_SQUARE);
+        Wave_SetFreq(DEFAULT_FREQ);
+        Wave_SetDuty(DEFAULT_DUTY);
+        Wave_SetOutput(1);
+        Wave_ResetIndex();
+    }
 
     LCD_Show_Status();
 
@@ -137,21 +285,25 @@ int main(void)
         {
             Wave_ToggleOutput();
             LCD_Show_Status();
+            Settings_MarkDirty();
         }
         else if (key == KEY_1)
         {
             Wave_Switch_Next();
             LCD_Show_Status();
+            Settings_MarkDirty();
         }
         else if (key == KEY_2)
         {
             Wave_SetFreq(wave_freq + FREQ_STEP);
             LCD_Show_Status();
+            Settings_MarkDirty();
         }
         else if (key == KEY_3)
         {
             Wave_SetFreq(wave_freq - FREQ_STEP);
             LCD_Show_Status();
+            Settings_MarkDirty();
         }
         else if (key == KEY_4)
         {
@@ -159,6 +311,7 @@ int main(void)
             {
                 Wave_SetDuty(square_duty + DUTY_STEP);
                 LCD_Show_Status();
+                Settings_MarkDirty();
             }
         }
         else if (key == KEY_5)
@@ -167,9 +320,11 @@ int main(void)
             {
                 Wave_SetDuty(square_duty - DUTY_STEP);
                 LCD_Show_Status();
+                Settings_MarkDirty();
             }
         }
 
+        Settings_Task();
         Delay_ms(30);
     }
 }
